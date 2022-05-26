fea7903daf6f11194164a9ef57a75900cc1ede13
Adonys Medina <60624032+AdonysProg@users.noreply.github.com>
2022-05-26 19:14:45 -0400

    MED-002: Added immutable for yarn installation (#2)
    * chore: added immutable yarn install for cache
    
    * chore: revert change
    
    * test
    
    * testing eslint pre-commit with husky
    
    * added lintstaged
    
    * added lint-taged to package scripts
    
    * chore: added pre-commit configuration
    
    * fix: fixed the commit-msg hook
    
    * chore: added commit lint
    
    * test: testing commitlint
    
    * fix: added rules for prefixes MED-002
    
    * fix: added yarn build in pre-commit MED-002
    
    * fix: prettier command was changed to check-format MED-002

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+/**
+ *
+ * @returns Home
+ */
+export default function Home() {
+  return (
+    <div>
+      <a target="_blank" href="https://youtu.be/dQw4w9WgXcQ" rel="noreferrer">
+        CI/CD - Click for more info.
+      </a>
+    </div>
+  );
+}
