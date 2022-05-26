98848479779cc4d044cb8f66ae11d166375b697d
Adonys Medina <adonysprogramming@gmail.com>
2022-05-26 16:10:53 -0400

    chore: added prettier and eslint configuration

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-    <Link href="https://youtu.be/dQw4w9WgXcQ">
-      <a target="_blank">CI/CD - Click for more info</a>
-    </Link>
+      <Link href="https://youtu.be/dQw4w9WgXcQ">
+        <a target="_blank">CI/CD - Click for more info</a>
+      </Link>
     </div>
-  )
+  );
 }
