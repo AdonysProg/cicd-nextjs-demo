ee931ce1c2db498cc43a0cc919657e0405ed3686
Adonys Medina <adonysprogramming@gmail.com>
2022-05-26 16:22:01 -0400

    added yarn lint

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Link href="https://youtu.be/dQw4w9WgXcQ">
-        <a target="_blank">CI/CD - Click for more info</a>
+  <a target="_blank">CI/CD - Click for more info.</a>
       </Link>
     </div>
   );
