648540ac4d188995d3f5ce982e6f2bbaa0d64118
Adonys Medina <adonysprogramming@gmail.com>
2022-05-26 17:58:19 -0400

    test

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <a target="_blank" href="https://youtu.be/dQw4w9WgXcQ" rel="noreferrer">
+        CI/CD - Click for more info.
+      </a>
+    </div>
+  );
+}
