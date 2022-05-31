20a0febb90d5b0c873e4e66cee596dff2cd9c84e
Adonys Medina <adonysprogramming@gmail.com>
2022-05-31 12:37:56 -0400

    chore: testing MED-003

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@
 export default function Home() {
   return (
     <div>
- <a target="_blank" href="https://youtu.be/dQw4w9WgXcQ" rel="noreferrer">
- CI/CD - Click for more info
- </a>
+<a target="_blank" href="https://youtu.be/dQw4w9WgXcQ" rel="noreferrer">
+     CI/CD - Click for more info
+    </a>
     </div>
   );
 }
